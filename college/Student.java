5d8aa628644f55128e773361fafb9802e8fa36bd
Suvojoti Howlader <152045641+SuvoH05@users.noreply.github.com>
2026-09-25 03:44:31 +0530

    Understood packages!

---
+++ college/Student.java
@@ -0,0 +1,8 @@
+package college;
+
+public class Student {
+
+    public void study() {
+        System.out.println("Student is studying");
+    }
+}
---
+++ packagee.java
@@ -0,0 +1,16 @@
+import college.Student;
+
+public class packagee {
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+        s.study();
+    }
+}
+
+
+// "What is a package?"
+
+// A package is a mechanism in Java used to group related classes and interfaces, organize code, avoid naming conflicts, and provide access control.
